839a2b1ff7b3b3bfb47dcf44ec3d0645e7f5c9fa
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-05-04 09:23:53 -0300

    forEach com array

--- js/script.js
+++ js/script.js
@@ -34,39 +34,39 @@
 // //QUAL NOME SERÁ IMPRESSO QUANDO NOME FOR ADICIONADO AO LOG?
 // console.log(nome);
 
-let fruta = "melão";
-console.log(fruta);
+// let fruta = "melão";
+// console.log(fruta);
 
-//ARRAY são Collections
-let frutas = ["uva","pêra","melância"];
-console.log(frutas[0]);
-console.log(frutas[1]);
-console.log(frutas[2]);
-console.log("TAMANHO DO ARRAY : " + frutas.length);
-// let nome = "Leonardo";
-// console.log(nome[1]);
+// //ARRAY são Collections
+// let frutas = ["uva","pêra","melância"];
+// console.log(frutas[0]);
+// console.log(frutas[1]);
+// console.log(frutas[2]);
+// console.log("TAMANHO DO ARRAY : " + frutas.length);
+// // let nome = "Leonardo";
+// // console.log(nome[1]);
 
-//Adicionando um novo elemento no final do array
-//Utilizando o método push(item);
-frutas.push("banana");
-frutas.push("laranja");
-// console.log(frutas);
-console.table(frutas);
+// //Adicionando um novo elemento no final do array
+// //Utilizando o método push(item);
+// frutas.push("banana");
+// frutas.push("laranja");
+// // console.log(frutas);
+// console.table(frutas);
 
-//Removendo um item do final do array com o método pop();
-frutas.pop();
-console.table(frutas);
+// //Removendo um item do final do array com o método pop();
+// frutas.pop();
+// console.table(frutas);
 
-//Removendo um item do início do array com o método shift();
-frutas.shift();
-console.table(frutas);
+// //Removendo um item do início do array com o método shift();
+// frutas.shift();
+// console.table(frutas);
 
 //Adicionando um novo elemento no início do array
-//Utilizando o método unshift(item);
-frutas.unshift("maracujá");
-frutas.unshift("kiwi");
-// console.log(frutas);
-console.table(frutas);
+// //Utilizando o método unshift(item);
+// frutas.unshift("maracujá");
+// frutas.unshift("kiwi");
+// // console.log(frutas);
+// console.table(frutas);
 
 
 //Realizar uma busca de um item no array utiliza o método indexOf(nomeItem);
@@ -74,6 +74,16 @@ console.table(frutas);
 // console.log(frutas[indice]);
 
 //Removendo item através do indicie do array com método splice(indice, qtd)
-let indice = frutas.indexOf("pêra");
-frutas.splice(indice,1);
-console.table(frutas);
+// let indice = frutas.indexOf("pêra");
+// frutas.splice(indice,1);
+// console.table(frutas);
+
+//Iterando arrays
+// let frutas = ["uva","laranja","banana","maçã","kiwi","abacate"];
+// console.log("MINHAS FRUTAS : " + frutas);
+
+
+// frutas.forEach( (fruta)=>{
+//     console.log(fruta);
+// } );
+
